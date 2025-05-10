8a683c2d6e2397fc06cfb15edcdfe3894ca081a6
Hoseda <hoseda97@gmail.com>
2025-05-10 18:24:39 +0330

    add more item to home page

---
+++ lib/src/core/constants/home_page_constant.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart' show Icons;
+
+final homePageConstant = [
+  {
+    "text": "Habits",
+    "subtext" : "Tracking you habits",
+    "icon": Icons.bar_chart_rounded,
+  },
+  {
+    "text": "Tips",
+    "subtext" : "Tips of the day",
+    "icon": Icons.lightbulb_rounded,
+  },
+];
+
+
+final homePageGreenFuture = [
+  {
+    "text": "Green Revolution By Using Bicycle",
+    "desc" : "Bicycle produce zero emissions while in use, helping to reduce greenhouse gas emissoins that contribute  to global warming.",
+    "image": "assets/icons/bicycle.png",
+    "help" : "help1",
+    "point" : 500,
+  },
+  {
+    "text": "Green Revolution By Using Bicycle",
+    "desc" : "Bicycle produce zero emissions while in use, helping to reduce greenhouse gas emissoins that contribute  to global warming.",
+    "image": "assets/icons/bicycle.png",
+    "help" : "help1",
+    "point" : 500,
+  },
+  {
+    "text": "Green Revolution By Using Bicycle",
+    "desc" : "Bicycle produce zero emissions while in use, helping to reduce greenhouse gas emissoins that contribute  to global warming.",
+    "image": "/assets/icons/bicycle.png",
+    "help" : "help1",
+    "point" : 500,
+  },
+];
--- lib/src/features/home/presentation/home_page.dart
+++ lib/src/features/home/presentation/home_page.dart
@@ -1,6 +1,6 @@
+import 'package:eco_tracker/src/core/constants/home_page_constant.dart';
 import 'package:flutter/material.dart';
 import 'package:eco_tracker/src/core/constants/colors.dart';
-import 'package:table_calendar/table_calendar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,7 +12,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    final dat = DateTime(2025, 2, 10);
     return SafeArea(
       child: Scaffold(
         backgroundColor: backgound,
@@ -30,137 +29,316 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          "Hello , Maria Zorah!",
-                          style: TextStyle(
-                            fontFamily: "psans",
-                            fontSize: 19,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w500,
-                          ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        // todo : user name
+                        "Hello , Maria Zorah!",
+                        style: TextStyle(
+                          fontFamily: "psans",
+                          fontSize: 19,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w500,
                         ),
-                        Text(
-                          "Good Morning",
-                          style: TextStyle(
-                            fontFamily: "psans",
-                            color: iconbg,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      width: 90,
-                      height: 35,
-                      decoration: BoxDecoration(
-                        color: button,
-                        borderRadius: BorderRadius.circular(50),
                       ),
+                      Text(
+                        // todo : greeting base on time
+                        "Good Morning",
+                        style: TextStyle(
+                          fontFamily: "psans",
+                          color: iconbg,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    height: 35,
+                    decoration: BoxDecoration(
+                      color: button,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 7),
                       child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Icon(Icons.paid, color: primary),
-                          const Text(
+                          const SizedBox(width: 10),
+                          Text(
+                            // todo : user points
                             "2,012",
                             style: TextStyle(
                               fontFamily: "psans",
                               color: Colors.white,
+                              fontSize: 15,
                               fontWeight: FontWeight.w500,
                             ),
                           ),
                         ],
                       ),
                     ),
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 25),
-                  child: Container(
-                    width: double.infinity,
-                    height: 155,
-                    decoration: BoxDecoration(
-                      color: primary,
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const Text(
-                                "Weekly Challenges !!!",
-                                style: TextStyle(
-                                  fontFamily: "psans",
-                                  fontSize: 19,
-                                  fontWeight: FontWeight.w600,
-                                ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 25),
+                child: Container(
+                  width: double.infinity,
+                  height: 155,
+                  decoration: BoxDecoration(
+                    color: primary,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Weekly Challenges !!!",
+                              style: TextStyle(
+                                fontFamily: "psans",
+                                fontSize: 19,
+                                fontWeight: FontWeight.w600,
                               ),
-                              const Text(
-                                "Win and get Voucher",
+                            ),
+                            const Text(
+                              "Win and get Voucher",
+                              style: TextStyle(
+                                fontFamily: "psans",
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Container(
+                            width: 95,
+                            height: 33,
+                            decoration: BoxDecoration(
+                              color: button,
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Start now",
                                 style: TextStyle(
+                                  color: primary,
                                   fontFamily: "psans",
+                                  fontSize: 15,
                                   fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
-                          GestureDetector(
-                            onTap: () {},
-
-                            child: Container(
-                              width: 95,
-                              height: 33,
-                              decoration: BoxDecoration(
-                                color: button,
-                                borderRadius: BorderRadius.circular(30),
-                              ),
-                              child: Center(
-                                child: Text(
-                                  "Start now",
-                                  style: TextStyle(
-                                    color: primary,
-                                    fontFamily: "psans",
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w500,
-                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-                TableCalendar(
-                  focusedDay: dat,
-                  firstDay: dat,
-                  lastDay: dat,
-                  calendarFormat: CalendarFormat.week,
-                  headerVisible: false,
-                  weekendDays: [DateTime.saturday],
-                  startingDayOfWeek: StartingDayOfWeek.saturday,
-                  onDaySelected: (selectedDay, focusedDay) {
+              ),
+              // todo : add horizontal scrollable calendar here.
+              // ----------
+              const SizedBox(height: 10),
+              SizedBox(
+                height: 150,
+                child: Expanded(
+                  child: GridView.builder(
+                    scrollDirection: Axis.vertical,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: 2,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 15,
+                      mainAxisExtent: 133,
+                    ),
+                    itemBuilder: (context, index) {
+                      final shadow = homePageConstant;
+                      final icon = shadow[index]["icon"] as IconData;
+                      final text = shadow[index]["text"] as String;
+                      final subtext = shadow[index]["subtext"] as String;
 
-                  },
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: button,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Icon(icon, color: primary, size: 28),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    text,
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                  Text(
+                                    subtext,
+                                    style: TextStyle(
+                                      color: iconbg,
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w400,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                "Step into a Green Future",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                height: 170,
+                child: Expanded(
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 2,
+                    itemBuilder: (context, index) {
+                      final shadow = homePageGreenFuture;
+                      final text = shadow[index]["text"] as String;
+                      final desc = shadow[index]["desc"] as String;
+                      final image = shadow[index]["image"] as String;
+                      final help = shadow[index]["help"] as String;
+                      final point = shadow[index]["point"].toString();
+
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Container(
+                          width: 310,
+                          decoration: BoxDecoration(
+                            color: button,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: 15,
+                              vertical: 15,
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      text,
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    // todo : use help variable in here for ? icon
+                                    Icon(
+                                      Icons.help_outline_rounded,
+                                      size: 20,
+                                      color: iconbg,
+                                    ),
+                                  ],
+                                ),
+                                const SizedBox(height: 5),
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Image.asset(image, scale: 7),
+                                    Expanded(
+                                      child: Text(
+                                        desc,
+                                        softWrap: true,
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          color: iconbg,
+                                        ),
+                                        textAlign: TextAlign.justify,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Container(
+                                      height: 30,
+                                      decoration: BoxDecoration(
+                                        color: primary,
+                                        borderRadius: BorderRadius.circular(50),
+                                      ),
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                                        child: Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            Icon(Icons.paid, color: secondary),
+                                            const SizedBox(width: 5),
+                                            Text(
+                                              // todo : user points
+                                              "+$point",
+                                              style: TextStyle(
+                                                fontFamily: "psans",
+                                                color: secondary,
+                                                fontSize: 14,
+                                                fontWeight: FontWeight.w500,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
