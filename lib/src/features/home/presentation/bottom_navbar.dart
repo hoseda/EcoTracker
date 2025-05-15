fcddce5fd62c6b369da0d38b435516a936c49ddd
Hoseda <hoseda97@gmail.com>
2025-05-15 13:35:22 +0330

    finishing the simple bottom navigation bar

--- lib/src/features/home/presentation/bottom_navbar.dart
+++ lib/src/features/home/presentation/bottom_navbar.dart
@@ -1,8 +1,38 @@
 import 'package:eco_tracker/src/core/constants/colors.dart';
 import 'package:flutter/material.dart';
 
-class BottomNavbar extends StatelessWidget {
+class BottomNavbar extends StatefulWidget {
   const BottomNavbar({super.key});
+
+  @override
+  State<BottomNavbar> createState() => _BottomNavbarState();
+}
+
+class _BottomNavbarState extends State<BottomNavbar> {
+  var isSelected = [true, false, false, false];
+
+  Widget generateBottomNavbarItem(IconData icon, int indx) {
+    final color = isSelected[indx] ? primary : iconbg;
+    return GestureDetector(
+      onTap: () {
+        if (!isSelected[indx]) {
+          setState(() {
+            isSelected = List.filled(isSelected.length, false, growable: false);
+            isSelected[indx] = true;
+          });
+        }
+      },
+      child: Container(
+        width: 60,
+        height: 60,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: color,
+        ),
+        child: Icon(icon, color: Colors.white),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,12 +41,19 @@ class BottomNavbar extends StatelessWidget {
       child: Container(
         height: 70,
         decoration: BoxDecoration(
-          color: Color.fromARGB(255, 14, 13, 13),
+          color: button,
           borderRadius: BorderRadius.circular(50),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            generateBottomNavbarItem(Icons.home_rounded, 0),
+            generateBottomNavbarItem(Icons.groups_2_rounded, 1),
+            generateBottomNavbarItem(Icons.calculate_rounded, 2),
+            generateBottomNavbarItem(Icons.person, 3),
+          ],
         ),
       ),
     );
   }
 }
-
-
