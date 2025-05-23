f7dd1a21ba3f4aeb2c805829204947ce2c1e884c
Hoseda <hoseda97@gmail.com>
2025-05-23 22:13:25 +0330

    adding ui of the today's quote section , and adding ui of the other setting section  but its not completed.

--- lib/src/core/constants/profile_page_constant.dart
+++ lib/src/core/constants/profile_page_constant.dart
@@ -1,5 +1,10 @@
 final profileCardConstants = [
-  {'text': 'Eco points','point' : '2,012' , 'image' : 'assets/icons/coins.svg'},
-  {'text': 'Stars','point' : '500' , 'image' : 'assets/icons/star.svg'},
-  {'text': 'Vouchers','point' : '11' , 'image' : 'assets/icons/ticket.svg'},
+  {'text': 'Eco points', 'point': '2,012', 'image': 'assets/icons/coins.svg'},
+  {'text': 'Stars', 'point': '500', 'image': 'assets/icons/star.svg'},
+  {'text': 'Vouchers', 'point': '11', 'image': 'assets/icons/ticket.svg'},
+];
+
+final profileSettings = [
+  {'title' : 'Dashboard' , 'desc' : 'Your progress tracker' , 'icon' : 'assets/icons/chart.svg'},
+  {'title' : 'My Activity' , 'desc' : 'Your enviromental actions' , 'icon' : 'assets/icons/diary.svg'},
 ];
--- lib/src/features/profile/presentation/profile.dart
+++ lib/src/features/profile/presentation/profile.dart
@@ -49,60 +49,136 @@ class _ProfileState extends State<Profile> {
           ),
           const SizedBox(height: 15),
           SizedBox(
-                height: 115,
-                child: GridView.builder(
-                  scrollDirection: Axis.horizontal,
-                  physics: NeverScrollableScrollPhysics(),
-                  itemCount: 3,
-                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 115,
-                    mainAxisSpacing: 7,
-                  ),
-                  itemBuilder: (context, index) {
-                    final shadow = profileCardConstants;
-                    final image = shadow[index]["image"];
-                    final text = shadow[index]["text"] as String;
-                    final point = shadow[index]["point"] as String;
+            height: 110,
+            child: GridView.builder(
+              scrollDirection: Axis.horizontal,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: 3,
+              gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 115,
+                mainAxisSpacing: 7,
+                mainAxisExtent: 115,
+              ),
+              itemBuilder: (context, index) {
+                final shadow = profileCardConstants;
+                final image = shadow[index]["image"];
+                final text = shadow[index]["text"] as String;
+                final point = shadow[index]["point"] as String;
 
-                    return Container(
-                      decoration: BoxDecoration(
-                        color: button,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(15),
-                        child: Column(
+                return Container(
+                  decoration: BoxDecoration(
+                    color: button,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        SvgPicture.asset(
+                          image!,
+                          colorFilter: ColorFilter.mode(
+                            primary,
+                            BlendMode.srcIn,
+                          ),
+                          width: 28,
+                          height: 28,
+                        ),
+                        Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            SvgPicture.asset(image! , colorFilter: ColorFilter.mode(primary, BlendMode.srcIn),width: 28,height: 28,),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  point,
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 14,
-                                  ),
-                                ),
-                                Text(
-                                  text,
-                                  style: TextStyle(
-                                    color: iconbg,
-                                    fontSize: 12,
-                                    fontWeight: FontWeight.w400,
-                                  ),
-                                ),
-                              ],
+                            Text(
+                              point,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              text,
+                              style: TextStyle(
+                                color: iconbg,
+                                fontSize: 11,
+                                fontWeight: FontWeight.w400,
+                              ),
                             ),
                           ],
                         ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 15),
+            child: Container(
+              height: 120,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: primary,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 15,
+                  horizontal: 30,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      "Today's quote",
+                      style: TextStyle(color: iconbg, fontSize: 13),
+                    ),
+                    Text(
+                      "'Living sustainably isn't just about saving the planet - it's about creating a future where both nature and humanity thrive togather.'",
+                      softWrap: true,
+                      textAlign: TextAlign.justify,
+                      style: TextStyle(
+                        color: button,
+                        fontSize: 13,
+                        fontWeight: FontWeight.w600,
                       ),
-                    );
-                  },
+                    ),
+                  ],
                 ),
               ),
+            ),
+          ),
+          Row(
+            children: [
+              const Text(
+                "Details",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount:3,
+              scrollDirection: Axis.vertical,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                  child: Container(
+                    height: 60,
+                    width: double.maxFinite,
+                    color: Colors.amber,
+                  ),
+                );
+              },
+            ),
+          ),
         ],
       ),
     );
