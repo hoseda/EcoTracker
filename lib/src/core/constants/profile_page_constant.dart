d52d13339889b643f6d27e744e1cfec9b50e5724
Hoseda <hoseda97@gmail.com>
2025-05-23 21:16:49 +0330

    add ui of rewards and voucher in profile page

---
+++ lib/src/core/constants/profile_page_constant.dart
@@ -0,0 +1,5 @@
+final profileCardConstants = [
+  {'text': 'Eco points','point' : '2,012' , 'image' : 'assets/icons/coins.svg'},
+  {'text': 'Stars','point' : '500' , 'image' : 'assets/icons/star.svg'},
+  {'text': 'Vouchers','point' : '11' , 'image' : 'assets/icons/ticket.svg'},
+];
--- lib/src/features/profile/presentation/profile.dart
+++ lib/src/features/profile/presentation/profile.dart
@@ -1,6 +1,8 @@
 import 'package:eco_tracker/src/core/constants/colors.dart';
+import 'package:eco_tracker/src/core/constants/profile_page_constant.dart';
 import 'package:eco_tracker/src/features/profile/presentation/profile_avatar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -13,7 +15,7 @@ class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10),
+      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
       child: Column(
         children: [
           ProfileAvatar(),
@@ -30,11 +32,77 @@ class _ProfileState extends State<Profile> {
           ),
           Text(
             '"small action , big impact."',
-            style: TextStyle(
-              color: iconbg,
-              fontSize: 13,
-            ),
+            style: TextStyle(color: iconbg, fontSize: 13),
           ),
+          const SizedBox(height: 45),
+          Row(
+            children: [
+              Text(
+                'Rewards and Vouchers',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 15),
+          SizedBox(
+                height: 115,
+                child: GridView.builder(
+                  scrollDirection: Axis.horizontal,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: 3,
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                    maxCrossAxisExtent: 115,
+                    mainAxisSpacing: 7,
+                  ),
+                  itemBuilder: (context, index) {
+                    final shadow = profileCardConstants;
+                    final image = shadow[index]["image"];
+                    final text = shadow[index]["text"] as String;
+                    final point = shadow[index]["point"] as String;
+
+                    return Container(
+                      decoration: BoxDecoration(
+                        color: button,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            SvgPicture.asset(image! , colorFilter: ColorFilter.mode(primary, BlendMode.srcIn),width: 28,height: 28,),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  point,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                Text(
+                                  text,
+                                  style: TextStyle(
+                                    color: iconbg,
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
         ],
       ),
     );
